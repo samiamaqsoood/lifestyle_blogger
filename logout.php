d8b8797dcf462e3cdedbe58e6665c08c8b37e0d6
Hira.s <hira.s@turing.com>
2025-12-28 16:55:35 +0500

    logout

--- logout.php
+++ logout.php
@@ -1,10 +1,15 @@
 <?php
 require_once 'includes/config.php';
+require_once 'includes/functions.php';
 
-// Clear user session
+// Clear user session variables
 unset($_SESSION['user_id']);
 unset($_SESSION['user_username']);
 unset($_SESSION['user_fullname']);
+
+// Clear admin session variables (if exists)
+unset($_SESSION['admin_id']);
+unset($_SESSION['admin_username']);
 
 // Clear remember me cookie
 if (isset($_COOKIE['user_login'])) {
